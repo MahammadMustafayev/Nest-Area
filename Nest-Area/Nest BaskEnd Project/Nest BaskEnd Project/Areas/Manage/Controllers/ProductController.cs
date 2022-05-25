012164312a5f43d798456d937edc9d9c17b597cf
MahammadMustafayev <99835006+MahammadMustafayev@users.noreply.github.com>
2022-05-25 06:30:00 +0300

    added

---
+++ Nest-Area/Nest BaskEnd Project/Nest BaskEnd Project/Areas/Manage/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nest_BaskEnd_Project.Areas.Manage.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Area("Manage")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nest-Area/Nest BaskEnd Project/Nest BaskEnd Project/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nest_BaskEnd_Project.DAL;
+using Nest_BaskEnd_Project.Models;
+using Nest_BaskEnd_Project.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nest_BaskEnd_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get;  }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Product> query = _context.Products.Include(p => p.ProductImages).Include(p => p.Category).AsQueryable();
+            HomeVM homeVM = new HomeVM()
+            {
+                Sliders = await _context.Sliders.ToListAsync(),
+                Categories = await _context.Categories.Where(c=>c.IsDeleted==false).ToListAsync(),
+                Products = await query.Take(10).ToListAsync(),
+                RecentlyAdded= await query.OrderByDescending(p=>p.Id).Take(3).ToListAsync(),
+                TopRated= await query.OrderByDescending(p=>p.Raiting).Take(3).ToListAsync()
+            };
+            return View(homeVM);
+        }
+    }
+}
